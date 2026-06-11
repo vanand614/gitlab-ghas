7965e26122317b95d3d1e44bbb407ab3828f874c
Viswanath Anand <vanand@slideinsurance.com>
2026-06-11 18:52:31 +0530

    fixed the syntax issue

--- scripts/wait-for-dependency-review.ps1
+++ scripts/wait-for-dependency-review.ps1
@@ -1,40 +1,38 @@
 $Headers = @{
     Authorization = "Bearer $env:GITHUB_TOKEN"
-    Accept = "application/vnd.github+json"
+    Accept        = "application/vnd.github+json"
 }
- 
+
 $Branch = $env:CI_COMMIT_REF_NAME
- 
+
 Write-Host "Waiting for Dependency Review..."
- 
-for($i=1;$i -le 30;$i++)
-{
-    $Runs = Invoke-RestMethod `
-      -Uri "https://api.github.com/repos/$env:GITHUB_OWNER/$env:GITHUB_REPO/actions/workflows/dependency-review.yml/runs?branch=$Branch&per_page=1%22 `
-      -Headers $Headers
- 
-    if($Runs.workflow_runs.Count -eq 0)
-    {
-        Start-Sleep 10
+
+for ($i = 1; $i -le 30; $i++) {
+
+    $uri = "https://api.github.com/repos/$env:GITHUB_OWNER/$env:GITHUB_REPO/actions/workflows/dependency-review.yml/runs?branch=$Branch&per_page=1"
+
+    $Runs = Invoke-RestMethod -Uri $uri -Headers $Headers
+
+    if ($Runs.workflow_runs.Count -eq 0) {
+        Start-Sleep -Seconds 10
         continue
     }
- 
+
     $Latest = $Runs.workflow_runs[0]
- 
+
     Write-Host "Status: $($Latest.status)"
     Write-Host "Conclusion: $($Latest.conclusion)"
- 
-    if($Latest.status -eq "completed")
-    {
-        if($Latest.conclusion -eq "success")
-        {
+
+    if ($Latest.status -eq "completed") {
+        if ($Latest.conclusion -eq "success") {
             exit 0
         }
- 
+
         throw "Dependency Review Failed"
     }
- 
-    Start-Sleep 15
+
+    Start-Sleep -Seconds 15
 }
- 
+
 throw "Dependency Review Timeout"
+``
